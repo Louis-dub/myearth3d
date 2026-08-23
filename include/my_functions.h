f63bac9ceb11f0a414c6f5407cacfbd90347c172
Louis <louis.dubois@epitech.eu>
2026-08-23 11:32:45 +0200

    ADD: files .h

---
+++ include/my_functions.h
@@ -0,0 +1,13 @@
+/*
+** DUBOIS PROJECT, 2026
+** myearth3d
+** File description:
+** Functions prototypes
+*/
+
+#ifndef INCLUDED_MY_FUNCTIONS_H
+    #define INCLUDED_MY_FUNCTIONS_H
+
+
+
+#endif
---
+++ include/my_struct.h
@@ -0,0 +1,13 @@
+/*
+** DUBOIS PROJECT, 2026
+** myearth3d
+** File description:
+** Structures definitions
+*/
+
+#ifndef INCLUDED_MY_STRUCT_H
+    #define INCLUDED_MY_STRUCT_H
+
+
+
+#endif
