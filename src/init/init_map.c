78b0aae691e62d913c9699eee69e8c41fd1efe93
Louis <louis.dubois@epitech.eu>
2026-08-27 18:23:28 +0200

    ADD: init 2D map

---
+++ src/init/init_map.c
@@ -0,0 +1,24 @@
+/*
+** DUBOIS PROJECT, 2026
+** myearth3d
+** File description:
+** init map
+*/
+
+#include <stdlib.h>
+
+#include "my_struct.h"
+
+map_t *init_map()
+{
+    map_t *map = malloc(sizeof(map_t));
+
+    map->map_3d = malloc(sizeof(int*) * 15);
+    for (int i = 0; i < 15; i++) {
+        map->map_3d[i] = malloc(sizeof(int) * 15);
+        for (int j = 0; j < 15; j++)
+            map->map_3d[i][j] = 0;
+    }
+    map->map_2d = malloc(sizeof(sfVector2f) * 15 * 15);
+    return map;
+}
