7012319f59fcc34ecabcc3287be86c0993a545a8
Louis <louis.dubois@epitech.eu>
2026-08-28 12:59:49 +0200

    FIX: projection calculation

--- src/display_map/calculate_map2d.c
+++ src/display_map/calculate_map2d.c
@@ -13,8 +13,8 @@ void calculate_map2d(map_t *map)
 {
     for (int i = 0; i < map->size; i++) {
         for (int j = 0; j < map->size; j++) {
-            map->map_2d[i][j].x = cosf(map->angle) * i - cosf(map->angle) * j;
-            map->map_2d[i][j].y = sinf(map->angle) * i + sinf(map->angle) * j + map->map_3d[i][j];
+            map->map_2d[i][j].x = (cosf(map->angle * M_PI / 180) * i - cosf(map->angle * M_PI / 180) * j) * 100 + 400;
+            map->map_2d[i][j].y = (sinf(map->angle * M_PI / 180) * i + sinf(map->angle * M_PI / 180) * j - map->map_3d[i][j]) * 100 + 300;
         }
     }
 }
