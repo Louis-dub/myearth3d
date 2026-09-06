5b3e0008f06f87850dc2b622052abd2c8694ee72
Louis <louis.dubois@epitech.eu>
2026-09-06 18:10:04 +0200

    ADD: sort squares

--- src/init/init_map.c
+++ src/init/init_map.c
@@ -12,6 +12,58 @@
 #include "my_functions.h"
 #include "my_struct.h"
 
+static square_t **fusion(square_t **left, square_t **right, int len)
+{
+    square_t **squares = malloc(sizeof(square_t*) * (len + 1));
+    int i_left = 0;
+    int i_right = 0;
+
+    squares[len] = NULL;
+    for (int i = 0; i < len; i++) {
+        if (!left[i_left]) {
+            squares[i] = right[i_right];
+            i_right++;
+        } else if (!right[i_right]) {
+            squares[i] = left[i_left];
+            i_left++;
+        } else if (left[i_left]->depth < right[i_right]->depth) {
+            squares[i] = left[i_left];
+            i_left++;
+        } else {
+            squares[i] = right[i_right];
+            i_right++;
+        }
+    }
+    free(left);
+    free(right);
+    return squares;
+}
+
+static square_t **create_copy(square_t **og, int len, int index)
+{
+    square_t **copy_square = malloc(sizeof(square_t*) * (len + 1));
+
+    copy_square[len] = NULL;
+    for (int i = 0; i < len; i++) {
+        copy_square[i] = og[index];
+        index++;
+    }
+    return copy_square;
+}
+
+static square_t **sort_squares(square_t **squares, int len)
+{
+    if (len == 1)
+        return squares;
+    square_t **left = create_copy(squares, len / 2, 0);
+    square_t **right = create_copy(squares, len - len / 2, len / 2);
+
+    left = sort_squares(left, len / 2);
+    right = sort_squares(right, len - len / 2);
+    free(squares);
+    return fusion(left, right, len);
+}
+
 static int set_depth(map_t *map, int i, int j, sfVector2u *size)
 {
     int depth = 0;
@@ -50,7 +102,7 @@ square_t **create_squares(map_t *map, sfVector2u *size)
             index++;
         }
     }
-    return squares;
+    return sort_squares(squares, len);
 }
 
 map_t *init_map(void)
