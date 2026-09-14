9553751ee81fb41bdfdcb11f9749b9917e6e02ad
Louis <louis.dubois@epitech.eu>
2026-09-14 20:31:12 +0200

    FIX: isometry projection

--- src/maths/project_iso_point.c
+++ src/maths/project_iso_point.c
@@ -15,7 +15,9 @@ sfVector2i project_iso_point(sfVector3f *cartesian, sphere_t *sphere, sfVector2u
 {
     sfVector2i point;
 
-    point.x = cartesian->y * sphere->zoom + size->x / 2.0;
-    point.y = -(cartesian->z * sphere->zoom) + size->y / 2.0;
+    point.x = (cartesian->x * (-sqrtf(3.0) / 2) + cartesian->y * (sqrtf(3.0) / 2)) * sphere->zoom;
+    point.y = (cartesian->x * (1.0 / 2) + cartesian->y * (1.0 / 2) - cartesian->z) * sphere->zoom;
+    point.x += size->x / 2.0;
+    point.y += size->y / 2.0;
     return point;
 }
