54f0520f2c5843906cb606c4c62fc742e47c5648
Louis <louis.dubois@epitech.eu>
2026-09-21 19:53:13 +0200

    SAVE

--- src/maths/project_iso_point.c
+++ src/maths/project_iso_point.c
@@ -43,7 +43,11 @@ static sfVector3f make_double_rotation(sfVector3f *cartesian, sphere_t *sphere)
                                           x, y, z, d1);
 
     rotate = make_one_rotation(&sphere->axes[1],
+<<<<<<< Updated upstream
                        rotate.x, rotate.y, rotate.z, d2);
+=======
+                               rotate.x, rotate.y, rotate.z, d2);
+>>>>>>> Stashed changes
     return rotate;
 }
 
